3228e05a3ff47d8ba72273e63009676fb4ee8b32
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2017-10-23 19:58:02 +0800

    1212

--- app/controllers/manager.js
+++ app/controllers/manager.js
@@ -3,6 +3,7 @@ var login = require('./login');
 var router = express.Router();
 const db = require("../db/");
 
+
 router.get('/login', function (req, res, next) {
 
     res.render('manager/login.ejs');
@@ -19,7 +20,7 @@ router.post('/login/index', function (req, res, next) {
     }).then(function (result) {
         if (result != null) {
             req.session.admin = result;
-            res.redirect("/manager/order");
+            res.redirect("/manager/test");
         } else {
             res.render('/manager/login');
         }
@@ -29,11 +30,11 @@ router.post('/login/index', function (req, res, next) {
 //访问管理页面
 router.get('/', login.checkin, function (req, res, next) {
 
-    db.User.findAll().then(function (result) {
+    db.AdminInfo.findAll().then(function (result) {
 
         res.render('manager/index.ejs', { users: result});
     });
-})
+});
 
 router.get('/user', login.checkin, function (req, res, next) {
 
@@ -41,7 +42,7 @@ router.get('/user', login.checkin, function (req, res, next) {
 
         res.render('manager/user.ejs', { users: result });
     });
-})
+});
 
 router.get('/role', login.checkin, function (req, res, next) {
 
@@ -49,7 +50,7 @@ router.get('/role', login.checkin, function (req, res, next) {
 
         res.render('manager/role.ejs', { roles: result });
     });
-})
+});
 
 router.get('/project', login.checkin, function (req, res, next) {
 
@@ -57,7 +58,13 @@ router.get('/project', login.checkin, function (req, res, next) {
 
         res.render('manager/project.ejs', { projects: result });
     });
-})
+});
+
+
+router.get('/test', login.checkin, function (req, res, next) {
+    
+    res.render('manager/test.ejs');
+    });
 
 router.get('/order', login.checkin, function (req, res, next) {
     
@@ -68,5 +75,5 @@ router.get('/order', login.checkin, function (req, res, next) {
         db.Sequelize.query("SELECT o.*,p.`name`,a.adminname from `order` as o LEFT JOIN product as p ON o.product_id = p.id LEFT JOIN admininfo as a ON a.id = o.adminInfo_id").then(function (result) {
             res.render('manager/order.ejs', { order: result[0] });
         });
-    })
+    });
 module.exports = router;
--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 var uuid = require('node-uuid');
+var fs = require('fs');
 const db = require("../app/db/");
 db.init();
 //
@@ -19,8 +20,35 @@ const login = require('../app/controllers/login');
 const weixinserver = require('../wechat/weixin');
 //
 var http = require('http');
+var multer  = require('multer')
 //
 var admin = require('../app/controllers/manager');
+
+var createFolder = function(folder){
+    try{
+        fs.accessSync(folder); 
+    }catch(e){
+        fs.mkdirSync(folder);
+    }  
+};
+
+var uploadFolder = './upload/';
+
+createFolder(uploadFolder);
+
+// 通过 filename 属性定制
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, uploadFolder);    // 保存的路径，备注：需要自己创建
+    },
+    filename: function (req, file, cb) {
+        // 将保存文件名设置为 字段名 + 时间戳，比如 logo-1478521468943
+        cb(null, file.fieldname + '-' + Date.now());  
+    }
+});
+
+// 通过 storage 选项来对 上传行为 进行定制化
+var upload = multer({ storage: storage })
 
 
 function getopenid(req, res, next) {
@@ -48,6 +76,16 @@ module.exports = function (app) {
         wechatApp.getAccessToken().then(function (data) {
             res.send(data);
         });
+    });
+
+    app.post('/uploadSingle', upload.single('logo'), function(req, res, next){
+        var file = req.file;
+        
+            console.log('文件类型：%s', file.mimetype);
+            console.log('原始文件名：%s', file.originalname);
+            console.log('文件大小：%s', file.size);
+            console.log('文件保存路径：%s', file.path);
+        res.send({ret_code: '0'});
     });
 
     //
