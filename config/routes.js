01ee5591893e18db1cac176b48d2e3c5acb403f8
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2017-10-25 16:22:42 +0800

    121212

--- config/routes.js
+++ config/routes.js
@@ -58,23 +58,29 @@ function getopenid(req, res, next) {
 
 module.exports = function (app) {
     db.init();
-    app.get('/testgetserver', function (req, res) {
-        home.testgetserver(req, res);
-    });
-    //
-    app.get('/wx', function (req, res) {
-        wechatApp.auth(req, res);
+
+
+    app.get('/', function (req, res) {
+        res.render('manager/login.ejs');
     });
 
     //
-    app.post('/wx', function (req, res) {
-        wechatApp.handleMsg(req, res);
-    });
-
-    //
-    app.get('/getAccessToken', function (req, res) {
-        wechatApp.getAccessToken().then(function (data) {
-            res.send(data);
+    //登陆
+    app.post('/login', function (req, res, next) {
+        var username = req.body.username;
+        var password = req.body.password;
+        db.AdminInfo.findOne({
+            where: {
+                adminname: username,
+                password: password
+            }
+        }).then(function (result) {
+            if (result != null) {
+                req.session.admin = result;
+                res.redirect("/manager/index");
+            } else {
+                res.render('manager/login.ejs');
+            }
         });
     });
 
@@ -151,149 +157,7 @@ module.exports = function (app) {
 
     });
 
-    app.get('/wx/test', getopenid, function (req, res, next) {
 
-
-        webchatTest.add(req.session.openid);
-    });
-
-    app.get('/wx/marketingregister', weixinserver.userinfo, function (req, res, next) {
-        
-                var weixin = req.session.weixin;
-        
-                console.log("get1", weixin);
-        
-                db.User.findOne({
-                    where: {
-                        openid: weixin.openid
-                    }
-                }).then(function (result) {
-                    if (result) {
-                        req.session.user = result;
-                    } 
-                    res.redirect("/index.html");
-                });
-        
-            });
-
-
-    app.get('/wx/userinfo', weixinserver.userinfo, function (req, res, next) {
-
-        var weixin = req.session.weixin;
-
-        console.log("get1", weixin);
-
-        db.User.findOne({
-            where: {
-                openid: weixin.openid
-            }
-        }).then(function (result) {
-            if (result) {
-                //
-                req.session.user = result;
-
-                if (result.mobile) {
-                    res.redirect('/user');
-                } else {
-                    console.log("find a person bu no register");
-                    if (result.roleid == 0) {
-                        res.redirect("/register.html");
-                    } else {
-                        res.redirect("/SaleRegister.html");
-                    }
-             
-                }
-          
-            } else {
-                console.log("find no person");
-                res.redirect("/register.html");
-            }
-        });
-
-    });
-    //
-    app.get('/user', function (req, res, next) {
-        var user = req.session.user;
-  
-        if (user != null) {
-            db.User.findOne({
-                where: {
-                    openid: user.openid
-                }
-            }).then(function (result) {
-                if (result.roleid == 0) {
-                    res.redirect("/registerEnd.html");
-                } else {
-                    res.render('home/salesperson.ejs', { user: result });
-                }
-            });
-        } else {
-            res.redirect("/register.html");
-        }
-    });
-
-    //
-    app.get('/user/my', function (req, res, next) {
-        var user = req.session.user;
-
-        if (user != undefined) {
-
-            db.Sequelize.query("select * from user as u1  join userinfo as u2 on u1.id=u2.user_id where u1.id='" + user.id+"'").then(function (fuser) {
-
-                var userf = fuser[0][0];
-
-                db.Sequelize.query("select * from user as u1  join userinfo as u2 on u1.id = u2.user_id where u1.sponsor='"+user.id+"'").then(function (sponsors) {
-
-                    res.render('home/user.ejs', { user: userf, sponsors: sponsors[0]});
-
-                });
-
-
-            });
-       
-       
-        } else {
-
-          res.redirect("/register.html");
-        }
-
-    });
-    //
-    app.get('/user/orcode', function (req, res, next) {
-
-        var user = req.session.user;
-        
-        if (user != undefined) {
-            console.log("jj:"+user.id);
-            http.get('http://www.faruxue1688.com/wx/qrcode?scenid=' + user.id, function (result) {
-                console.log("imgreq:"+'http://www.faruxue1688.com/wx/qrcode?scenid=' + user.id);
-                var html = '';
-                result.on('data', function (data) {
-                    console.log("imgurl:"+data);
-                    html += data;
-                    
-                    res.render('home/orcode.ejs', { res: html });
-                });
-                result.on('end', function (html) {
-                    console.info("creare or code");
-                });
-            });
-        } else {
-            http.get('http://www.faruxue1688.com/wx/qrcode?scenid=11', function (result) {
-
-                var html = '';
-                result.on('data', function (data) {
-                    html += data;
-
-                    res.render('home/orcode.ejs', { res: html });
-                });
-                result.on('end', function (html) {
-                    console.info("creare or code");
-                });
-   
-            });
-        }
-    });
 
     app.get('/wx/qrcode', weixinserver.qrcode);
 
@@ -302,124 +166,8 @@ module.exports = function (app) {
         next();
     });
 
-    app.post('/register', function (req, res, next) {
-        var username = req.body.username;
-        var address = req.body.address;
-        var mobile = req.body.mobile;
-        var code = req.body.code;
-        var weixin = req.session.weixin;
-        var user = null;
-        var userinfo = null;
-       
-        if (weixin) {
-
-    
-            user = {
-                mobile: mobile,
-                openid: weixin.openid,
-                active: true,
-            };
-     
-            db.User.findOne({
-                where: {
-                    openid: weixin.openid
-                }
-            }).then(function (result) {
-                if (result != null) {
-
-
-                    if (result.mobile!=null&&result.mobile != "") {
-                        var cuser = result;
-                        req.session.user = cuser;
-                        console.log("user存入日志1："+req.session.user);
-                        res.redirect('/user');
-                    } else {
-
-              
-    
-                    userinfo = {
-                        id:uuid.v1(),
-                        username: username,
-                        address: address,
-                        userId: result.id,
-                        loginIp:'127.0.0.1'
-                    };
-                    Promise.all([
-                        db.User.update(user, { where: { openid: weixin.openid} }),
-                        db.UserInfo.create(userinfo)
-                    ]).then(function (udata) {
-                        var user = result;
-                        req.session.user = user;
-                        req.session.userInfo = userinfo;
-                        console.log("user存入日志2："+req.session.user);
-                        console.log("userInfo存入日志2："+req.session.userInfo);
-                        console.log("create success and go to registerEnd"+user.roleid);
-                        // res.redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx459b286ba935a855&redirect_uri=http://www.faruxue1688.com/wx/userinfo&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect");
-                        // if(user.roleid == 1){
-                        //     //销售
-                        //     console.log("销售+++++++："+user.roleid);
-                        //     // res.render('home/salesperson', { user: result });
-                            
-                        //     res.render('home/salesperson.ejs', { user: result });
-                        //     // res.redirect('/salesperson');
-                        //     console.log("销售跳转了");
-                        // }else{
-                        //     console.log("用户+++++++："+user.roleid);
-                        //     res.redirect('/user');
-                        //     console.log("用户跳转了");
-                        // }
-
-
-                        res.write(user.roleid+"");
-                        console.log("user.roleid的值："+user.roleid);
-                        res.end();
-                        console.log("走完+++++++：zzzzz");
-                        }).catch(function (e) {
-                            // return res.redirect('/register.html');
-                            console.log(e);
-                            next(e);
-                        });
-
-                    }
-                };
-                });
-
-  
-        } else {
-            user = {
-                mobile: mobile,
-                active: true,
-            };
-            userinfo = {
-                username: username,
-                address: address,
-            };
-
-            Promise.all([
-                db.User.create(user),
-                db.UserInfo.create(userinfo),
-            ]).then(function (result) {
-                var user = result[0];
-                req.session.user = user;
-                console.log("create success and go to registerEnd");
-                
-                res.redirect('/user');
-                console.log("标记：");
-            }).catch(function (e) {
-                return res.redirect('/register.html');
-                next(e);
-            });
-        }
-
-   
-
-    });
-    app.use('/test',require("../app/controllers/testdata"));
-    app.use('/manager',require("../app/controllers/manager"));
-    app.use('/pano',require("../app/controllers/pano"));
-    app.use('/product',require("../app/controllers/product"));
-    app.use('/search',require("../app/controllers/search"));
     app.use('/scheme',require("../app/controllers/scheme"));
+    app.use('/equipment',require("../app/controllers/equipment"));
 
     app.use(function (err, req, res, next) {
         if (err.message
