b0490fc06490ffcff228e55d9f7a88424826d5b0
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2017-11-14 16:01:25 +0800

    cv

--- microservice/monitor/models/Project.js
+++ microservice/monitor/models/Project.js
@@ -1,44 +1,56 @@
 ﻿//项目表
 module.exports = function (sequelize, DataTypes) {
-    return sequelize.define('Project', {
-        id: { type: DataTypes.STRING(100), primaryKey: true, unique: true },
-        schemeId: {
-            type: DataTypes.STRING(100),
-            field: 'scheme_id',
-            allowNull: true,
-            comment: '方案Id'
-          },
-          equipmentId: {
-            type: DataTypes.STRING(100),
-            field: 'equipment_id',
-            allowNull: true,
-            comment: '设备Id'
-          },
-          designId: {
-            type: DataTypes.STRING(100),
-            field: 'design_id',
-            allowNull: true,
-            comment: '设计Id'
-          },
-          adminInfoId: {
-            type: DataTypes.STRING(100),
-            field: 'adminInfo_id',
-            allowNull: false,
-            comment: '项目创建人员Id'
-          },
-          name: { type: DataTypes.STRING(100), allowNull: true, comment: '项目名称' },
-          address: { type: DataTypes.STRING(100), allowNull: true, comment: '项目地址' },
-          auditstate: { type: DataTypes.STRING(10), allowNull: true, comment: '审核状态' },
-          progress: { type: DataTypes.STRING(10), allowNull: true, comment: '项目进度' },
-          // 1 为开始  2已开始  3生产中  4已完成  5终止
-    },
-        {
-            timestamps: true,
-            underscored: true,
-            paranoid: true,
-            freezeTableName: true,
-            tableName: 'project',
-            charset: 'utf8',
-            collate: 'utf8_general_ci'
-        });
+  return sequelize.define('Project', {
+      id: { type: DataTypes.STRING(100), primaryKey: true, unique: true },
+      schemeId: {
+          type: DataTypes.STRING(100),
+          field: 'scheme_id',
+          allowNull: true,
+          comment: '方案Id'
+        },
+        equipmentId: {
+          type: DataTypes.STRING(100),
+          field: 'equipment_id',
+          allowNull: true,
+          comment: '设备Id'
+        },
+        designId: {
+          type: DataTypes.STRING(100),
+          field: 'design_id',
+          allowNull: true,
+          comment: '设计Id'
+        },
+        adminInfoId: {
+          type: DataTypes.STRING(100),
+          field: 'adminInfo_id',
+          allowNull: false,
+          comment: '项目创建人员Id'
+        },
+        designerId: {
+          type: DataTypes.STRING(100),
+          field: 'designer_id',
+          allowNull: true,
+          comment: '设计人员Id'
+        },
+        equipmenterId: {
+          type: DataTypes.STRING(100),
+          field: 'equipmenter_id',
+          allowNull: true,
+          comment: '分配设备人员Id'
+        },
+        name: { type: DataTypes.STRING(100), allowNull: true, comment: '项目名称' },
+        address: { type: DataTypes.STRING(100), allowNull: true, comment: '项目地址' },
+        auditstate: { type: DataTypes.STRING(10), allowNull: true, comment: '审核状态' },
+        progress: { type: DataTypes.STRING(10), allowNull: true, comment: '项目进度' },
+        // 1 为开始  2已开始  3生产中  4已完成  5终止
+  },
+      {
+          timestamps: true,
+          underscored: true,
+          paranoid: true,
+          freezeTableName: true,
+          tableName: 'project',
+          charset: 'utf8',
+          collate: 'utf8_general_ci'
+      });
 }
