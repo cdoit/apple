1fa4f55879ba3a080dce4fd47193b76dd96380a4
张杰 <165590438@qq.com>
2018-01-18 15:34:14 +0800

    12

--- config/routematerial.js
+++ config/routematerial.js
@@ -7,10 +7,10 @@ var fs = require('fs');
 const db = require("../app/db/");
 //
 const config = require('../config.json')
-const wechat = require('../wechat/wechat');
-//
-var wechatApp = new wechat(config); 
 
 module.exports = function (app) {
     db.initmaterial();
+    app.get('/material', function (req, res) {
+        res.render('material/views/index.ejs');
+    });
 };
