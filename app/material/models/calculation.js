3ae033714fc204eb3f59fe993270e0166213f050
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2018-01-18 10:19:11 +0800

    23

--- app/material/models/calculation.js
+++ app/material/models/calculation.js
@@ -7,7 +7,7 @@ module.exports = function (sequelize, DataTypes) {
       comment: '物料Id'
     },
     name: { type: DataTypes.STRING(200), allowNull: true, comment: ' ' },
-    projectid: { type: DataTypes.STRING(200), allowNull: true, comment: ' ' },
+    projectid: { type: DataTypes.STRING(200), allowNull: true, comment: '' },
     packgeid: { type: DataTypes.STRING(200), allowNull: true, comment: ' ' },
     num: { type: DataTypes.STRING(200), allowNull: true, comment: ' ' }
   },
