83445e77516e437b58fa0320e88dc338db9accd4
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2018-01-19 17:49:08 +0800

    12

--- config/routematerial.js
+++ config/routematerial.js
@@ -6,11 +6,21 @@ var uuid = require('node-uuid');
 var fs = require('fs');
 const db = require("../app/db/");
 //
-const config = require('../config.json')
+const config = require('../config.json');
+const wechat = require('../wechat/wechat');
+//
+var wechatApp = new wechat(config); 
 
 module.exports = function (app) {
     db.initmaterial();
     app.get('/material', function (req, res) {
         res.render('material/views/index.ejs');
     });
+
+    app.use('/material/supplybook',require("../app/material/controllers/supplybook"));
+
+
+    
+
+
 };
