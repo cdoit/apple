1a664d18d9971a95df2ac135b703196d057d6e28
张杰 <165590438@qq.com>
2018-01-29 17:34:14 +0800

    12

--- app/dingtalk/controllers/auth.js
+++ app/dingtalk/controllers/auth.js
@@ -4,11 +4,11 @@ var uuid = require('node-uuid');
 var querystring = require('querystring');
 var httpUtil = require('../../util/http');
 
-//供应商列表
+//供应商列表req.corpIdreq.corpsecret
 router.get('/gettoken' , function (req, res, next) { 
     var path = 'oapi.dingtalk.com/gettoken?' + querystring.stringify({
-        corpId: req.corpId,
-        corpsecret: req.corpsecret
+        corpId: "ding865f2022dc64284135c2f4657eb6378f",
+        corpsecret: "Bo67en-DR4aBMYHV-BczWlIWiFFa_aAla5kZbyc9JGwcHs6g2K2TWrtbZ1GWWqIH"
       });
       console.log("url:"+path);
       httpUtil.get(path, res);
