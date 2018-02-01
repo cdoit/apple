50a91fbc7b36658d8cc4ba8cf09450aa84611bf9
ZhangxfKKKP <zhang.xiangfei@cdoplus.com>
2018-02-01 15:30:03 +0800

    eeee

--- app/sso/controllers/index.js
+++ app/sso/controllers/index.js
@@ -7,16 +7,22 @@ var https = require('https');
 var cdohttps = require('../../uitl/cdohttps');
 
 //物料价格列表
-router.get('/' , function (req, res, next) { 
-    var path = 'https://oapi.dingtalk.com/gettoken?' + querystring.stringify({
-        corpid: 'ding865f2022dc64284135c2f4657eb6378f',
-        corpsecret: 'Bo67en-DR4aBMYHV-BczWlIWiFFa_aAla5kZbyc9JGwcHs6g2K2TWrtbZ1GWWqIH'});
-        new cdohttps().requestGet(path).then(function(data){
-            var obj = JSON.parse(data); 
-            req.session.token = obj.access_token;
-            // res.json(data);
-            res.render('sso/views/index.ejs',null);
-        });
+router.get('/' , function (req, res, next) {
+    var token = req.session.token;
+    if(token == null || token == undefined || token == ""){
+        var path = 'https://oapi.dingtalk.com/gettoken?' + querystring.stringify({
+            corpid: 'ding865f2022dc64284135c2f4657eb6378f',
+            corpsecret: 'Bo67en-DR4aBMYHV-BczWlIWiFFa_aAla5kZbyc9JGwcHs6g2K2TWrtbZ1GWWqIH'});
+            new cdohttps().requestGet(path).then(function(data){
+                var obj = JSON.parse(data); 
+                req.session.token = obj.access_token;
+                // res.json(data);
+                res.render('sso/views/index.ejs',null);
+            });
+    }else{
+        res.render('sso/views/index.ejs',null);
+    }
+    
     // res.render('sso/views/index.ejs',null);
 });
 
