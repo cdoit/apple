157925e0fbc31b984756318af505ae60669d8ad0
张杰 <165590438@qq.com>
2018-02-02 14:52:35 +0800

    12

--- app/sso/controllers/index.js
+++ app/sso/controllers/index.js
@@ -11,8 +11,8 @@ router.get('/' , function (req, res, next) {
     var token = req.session.token;
     if(token == null || token == undefined || token == ""){
         var path = 'https://oapi.dingtalk.com/gettoken?' + querystring.stringify({
-            corpid: 'ding865f2022dc64284135c2f4657eb6378f',
-            corpsecret: 'Bo67en-DR4aBMYHV-BczWlIWiFFa_aAla5kZbyc9JGwcHs6g2K2TWrtbZ1GWWqIH'});
+            corpid: 'ding865f2022dc64284135c2f4657eb6378f',//'ding865f2022dc64284135c2f4657eb6378f',
+            corpsecret: 'n9Y9w6csnHe936UYKAmBBaGNoivc-Zu-iLQV4pf4FP1uym-ekx_9WGMeohe4az8x'});//'Bo67en-DR4aBMYHV-BczWlIWiFFa_aAla5kZbyc9JGwcHs6g2K2TWrtbZ1GWWqIH'});
             new cdohttps().requestGet(path).then(function(data){
                 var obj = JSON.parse(data); 
                 req.session.token = obj.access_token;
