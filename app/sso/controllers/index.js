7577f6e7a31e789140e0ecbcbaaa1a4952605275
张杰 <165590438@qq.com>
2018-02-07 17:08:39 +0800

    12

--- app/sso/controllers/index.js
+++ app/sso/controllers/index.js
@@ -13,17 +13,17 @@ router.get('/' , function (req, res, next) {
         var path = 'https://oapi.dingtalk.com/gettoken?' + querystring.stringify({
             corpid: 'ding865f2022dc64284135c2f4657eb6378f',
             corpsecret: 'Bo67en-DR4aBMYHV-BczWlIWiFFa_aAla5kZbyc9JGwcHs6g2K2TWrtbZ1GWWqIH'});
-            new cdohttps().requestGet(path).then(function(data){
-                var obj = JSON.parse(data); 
-                req.session.token = obj.access_token;
-                // res.json(data);
-                res.render('sso/views/index.ejs',null);
-            });
-    }else{
+        new cdohttps().requestGet(path).then(function(data){
+            var obj = JSON.parse(data); 
+            //req.session.token = obj.access_token;
+            // res.json(data);
+            res.render('sso/views/index.ejs',null);
+        });
+    }
+    else
+    {
         res.render('sso/views/index.ejs',null);
     }
-    
-    // res.render('sso/views/index.ejs',null);
 });
 
 module.exports = router;
