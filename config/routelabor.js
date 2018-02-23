13a9968b0dc08d32b6ad173485c9de54fb76c43c
张杰 <165590438@qq.com>
2018-02-23 14:00:32 +0800

    12

--- config/routelabor.js
+++ config/routelabor.js
@@ -5,12 +5,8 @@ var router = express.Router();
 var uuid = require('node-uuid');
 var fs = require('fs');
 const db = require("../app/db/");
-//
-const config = require('../config.json');
-// const wechat = require('../wechat/wechat');
-//
-// var wechatApp = new wechat(config); 
 
 module.exports = function (app) {
-   
+    app.use('/cdo/labor',require("../app/labor/controllers/index"));
+ 
 };
